5143df8c694aadbc92967bf2f3a438253ffd5bed
BradRautenbach <54045202+BradRautenbach@users.noreply.github.com>
2019-08-18 16:00:54 +1200

    Modified the user class and started account manager

--- app/src/main/java/com/aut/parkit/AccountManager.java
+++ app/src/main/java/com/aut/parkit/AccountManager.java
@@ -1,6 +1,31 @@
 package com.aut.parkit;
 
+import java.util.Map;
+
 public class AccountManager {
 
+    private User user, modifiedUser;
+    private Map<Object, User> refrenceList;
+
+    public User getUser(Object caller){
+        User oldUser  = refrenceList.get(caller);
+        //User newUser = this.user.cl
+
+        if (oldUser != null){
+            oldUser.invalidate();
+            refrenceList.remove(oldUser);
+        }
+
+        refrenceList.put(caller, user);
+
+        return user;
+    }
+
+    public void pushModifiedUser(User user){
+       // modifiedUser = user
+    }
+
     //TODO: Finish
+
+
 }
--- app/src/main/java/com/aut/parkit/User.java
+++ app/src/main/java/com/aut/parkit/User.java
@@ -1,40 +1,43 @@
 package com.aut.parkit;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
-public class User {
+public class User implements Serializable {
     private String userID, accountType, firstName, lastName, emailAddress;
     private LinkedList<Vehicle> garage = new LinkedList<>();
     private Vehicle defaultVehicle;
     private boolean expireWarningNotification, breachNoticeNotification, dataModified;
     private ParkingSession currentParkingSession;
     private LinkedList<ParkingSession> parkingRecord = new LinkedList<>();
+    private boolean modified;
 
     private Map<String, Object> map;
     public static final String KEY_USERID = "UserID", KEY_ACCOUNTTYPE = "AccountType", KEY_FIRSTNAME = "FirstName", KEY_LASTNAME = "LastName",
-                KEY_EMAILADDRESS = "EmailAddress", KEY_EXPIREWARNINGNOTIFICATION = "ExpireWarningNotification",
-                KEY_BREACHNOTICENOTIFICATION = "BreachWarningNotification", KEY_DEFAULTVEHIVLE = "DefaultVehicleID";
+            KEY_EMAILADDRESS = "EmailAddress", KEY_EXPIREWARNINGNOTIFICATION = "ExpireWarningNotification",
+            KEY_BREACHNOTICENOTIFICATION = "BreachWarningNotification", KEY_DEFAULTVEHIVLE = "DefaultVehicleID";
 
     public User(String userID, String accountType) {
         this.userID = userID;
         this.accountType = accountType;
         this.dataModified = false;
+        this.modified = false;
     }
 
-    public void addVehicleToGarage(Vehicle vehicle){
+    public void addVehicleToGarage(Vehicle vehicle) {
         this.garage.add(vehicle);
     }
 
-    public void removeVehicleFromGarage(Vehicle vehicle){
+    public void removeVehicleFromGarage(Vehicle vehicle) {
         this.garage.remove(vehicle);
     }
 
-    public boolean removeVehicleFromGarage(String numberPlate){
-        for (Vehicle v: this.garage
-             ) {
-            if (v.getNumberPlate().contentEquals(numberPlate)){
+    public boolean removeVehicleFromGarage(String numberPlate) {
+        for (Vehicle v : this.garage
+        ) {
+            if (v.getNumberPlate().contentEquals(numberPlate)) {
                 this.garage.remove(v);
                 return true;
             }
@@ -43,7 +46,7 @@ public class User {
         return false;
     }
 
-    public Map<String, Object> toMap(){
+    public Map<String, Object> toMap() {
         this.map = new HashMap<>();
 
         this.map.put(User.KEY_USERID, this.userID);
@@ -56,6 +59,10 @@ public class User {
         this.map.put(User.KEY_DEFAULTVEHIVLE, this.defaultVehicle.getNumberPlate());
 
         return this.map;
+    }
+
+    public void invalidate(){
+        this.modified = true;
     }
 
     public void setFirstName(String firstName) {
@@ -90,43 +97,74 @@ public class User {
         return userID;
     }
 
-    public String getAccountType() {
+    public String getAccountType() throws Exception {
+        if (this.modified){
+            throw new Exception("User Object Obsolete");
+        }
         return accountType;
     }
 
-    public String getFirstName() {
+    public String getFirstName() throws Exception {
+        if (this.modified){
+            throw new Exception("User Object Obsolete");
+        }
         return firstName;
     }
 
-    public String getLastName() {
+    public String getLastName() throws Exception {
+        if (this.modified){
+            throw new Exception("User Object Obsolete");
+        }
         return lastName;
     }
 
-    public String getEmailAddress() {
+    public String getEmailAddress() throws Exception {
+        if (this.modified){
+            throw new Exception("User Object Obsolete");
+        }
         return emailAddress;
     }
 
-    public LinkedList<Vehicle> getGarage() {
+    public LinkedList<Vehicle> getGarage() throws Exception {
+        if (this.modified){
+            throw new Exception("User Object Obsolete");
+        }
         return garage;
     }
 
-    public Vehicle getDefaultVehicle() {
+    public Vehicle getDefaultVehicle() throws Exception {
+        if (this.modified){
+            throw new Exception("User Object Obsolete");
+        }
         return defaultVehicle;
     }
 
-    public boolean isExpireWarningNotification() {
+    public boolean isExpireWarningNotification() throws Exception {
+        if (this.modified){
+            throw new Exception("User Object Obsolete");
+        }
         return expireWarningNotification;
     }
 
-    public boolean isBreachNoticeNotification() {
+    public boolean isBreachNoticeNotification() throws Exception {
+        if (this.modified){
+            throw new Exception("User Object Obsolete");
+        }
         return breachNoticeNotification;
     }
 
-    public ParkingSession getCurrentParkingSession() {
+    public ParkingSession getCurrentParkingSession() throws Exception {
+        if (this.modified){
+            throw new Exception("User Object Obsolete");
+        }
         return currentParkingSession;
     }
 
-    public LinkedList<ParkingSession> getParkingRecord() {
+    public LinkedList<ParkingSession> getParkingRecord() throws Exception {
+        if (this.modified){
+            throw new Exception("User Object Obsolete");
+        }
+
         return parkingRecord;
     }
 }
