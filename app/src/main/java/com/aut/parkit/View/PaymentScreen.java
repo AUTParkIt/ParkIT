7faf0c7c507e9240aee598e60d2a4e73ea33722c
nlquill <52065976+nlquill@users.noreply.github.com>
2019-08-26 11:36:49 +1200

    Braintree dependencies deleted

--- app/src/main/java/com/aut/parkit/View/PaymentScreen.java
+++ app/src/main/java/com/aut/parkit/View/PaymentScreen.java
@@ -1,22 +1,18 @@
 package com.aut.parkit.View;
 
+import android.view.ViewGroup;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.content.ContextCompat;
 import com.aut.parkit.R;
-import com.braintreepayments.api.GooglePaymentActivity;
-import com.braintreepayments.api.dropin.DropInActivity;
-import com.braintreepayments.api.dropin.DropInRequest;
-import com.braintreepayments.api.dropin.DropInResult;
-import com.braintreepayments.api.dropin.utils.PaymentMethodType;
-import com.braintreepayments.api.models.PaymentMethodNonce;
+import com.google.android.material.appbar.AppBarLayout;
 
 public class PaymentScreen extends AppCompatActivity {
-
-    public static final int REQUEST_CODE = 1;
-    private String clientToken = "sandbox_388byrrp_nqhd7msq7bkx89wm";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,25 +20,4 @@ public class PaymentScreen extends AppCompatActivity {
         setContentView(R.layout.activity_payment_screen);
     }
 
-    public void onBraintreeSubmit(View v) {
-        DropInRequest dropInRequest = new DropInRequest()
-                .clientToken(clientToken)
-                .vaultManager(true);
-        startActivityForResult(dropInRequest.getIntent(this), REQUEST_CODE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                DropInResult result = data.getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT);
-                // use the result to update your UI and send the payment method nonce to your server
-            } else if (resultCode == RESULT_CANCELED) {
-                // the user canceled
-            } else {
-                // handle errors here, an exception may be available in
-                Exception error = (Exception) data.getSerializableExtra(DropInActivity.EXTRA_ERROR);
-            }
-        }
-    }
 }
