41e3b7b4bd42e6dcb219136fcad762be72d6b768
Tania <hvm1950@aut.ac.nz>
2019-09-03 10:20:28 +1200

    Modifed signup screen.  Added rounded corners to buttons.  Added file called custom button

---
+++ app/src/main/java/com/aut/parkit/View/SignupScreen.java
@@ -0,0 +1,14 @@
+package com.aut.parkit.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.aut.parkit.R;
+
+public class SignupScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_screen);
+    }
+}
