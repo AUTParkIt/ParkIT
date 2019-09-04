bbcb43b08ec587b01723f31414d7dd2c36a899aa
Tania <hvm1950@aut.ac.nz>
2019-09-04 22:15:38 +1200

    Modified Signup screen to add clickable text for Terms & Conditions

--- app/src/main/java/com/aut/parkit/View/SignupScreen.java
+++ app/src/main/java/com/aut/parkit/View/SignupScreen.java
@@ -3,8 +3,13 @@ package com.aut.parkit.View;
 import android.graphics.Color;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.aut.parkit.R;
@@ -16,16 +21,31 @@ public class SignupScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_screen);
 
-        TextView textView = findViewById(R.id.textView2);
-
+        TextView textView = findViewById(R.id.text_View2);
         String text = "CREATE AUT PARKIT ACCOUNT";
-
         SpannableString ss = new SpannableString(text);
-
         ForegroundColorSpan fcsBlack = new ForegroundColorSpan(Color.BLACK);
-
         ss.setSpan(fcsBlack, 15, 18, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
         textView.setText(ss);
+
+        CheckBox checkBox = findViewById(R.id.check_Box);
+        String clickableText = "I agree with the Terms & Conditions";
+        SpannableString ssClickable = new SpannableString(clickableText);
+        ClickableSpan clickableSpan = new ClickableSpan()
+        {
+            @Override
+            public void onClick(View widget)
+            {   //toast is a short popup message that pops up near the bottom of the screen
+                Toast.makeText(SignupScreen.this, "These are the Terms & Conditions", Toast.LENGTH_SHORT).show();
+
+                //this cancels the checkbox being checked if clicking on T&C's
+                widget.cancelPendingInputEvents();
+            }
+        };
+        ssClickable.setSpan(clickableSpan, 17, 35, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        checkBox.setText(ssClickable);
+        checkBox.setMovementMethod(LinkMovementMethod.getInstance());
+
+
     }
 }
