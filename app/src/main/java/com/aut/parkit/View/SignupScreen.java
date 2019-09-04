b85c972ef4618378ea4d0aab668d2d06fbc4d753
Tania <hvm1950@aut.ac.nz>
2019-09-05 10:56:15 +1200

    Modified clickable Go Back button

--- app/src/main/java/com/aut/parkit/View/SignupScreen.java
+++ app/src/main/java/com/aut/parkit/View/SignupScreen.java
@@ -7,28 +7,46 @@ import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.aut.parkit.R;
 
-public class SignupScreen extends AppCompatActivity {
+public class SignupScreen extends AppCompatActivity implements View.OnClickListener {
+
+    EditText firstName;
+    EditText lastName;
+    EditText email;
+    EditText password;
+    EditText confirmPassword;
+    CheckBox termsConditions;
+    CheckBox loggedIn;
+    Button signUp;
+    Button goBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_screen);
 
-        TextView textView = findViewById(R.id.text_View2);
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmPassword);
+        termsConditions = findViewById(R.id.termsConditions);
+        loggedIn = findViewById(R.id.loggedIn);
+        signUp = findViewById(R.id.signUp);
+        goBack = findViewById(R.id.goBack);
+
+        TextView screenTitle = findViewById(R.id.screenTitle);
         String text = "CREATE AUT PARKIT ACCOUNT";
         SpannableString ss = new SpannableString(text);
         ForegroundColorSpan fcsBlack = new ForegroundColorSpan(Color.BLACK);
         ss.setSpan(fcsBlack, 15, 18, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        textView.setText(ss);
+        screenTitle.setText(ss);
 
-        CheckBox checkBox = findViewById(R.id.check_Box);
+        CheckBox termsConditions = findViewById(R.id.termsConditions);
         String clickableText = "I agree with the Terms & Conditions";
         SpannableString ssClickable = new SpannableString(clickableText);
         ClickableSpan clickableSpan = new ClickableSpan()
@@ -38,14 +56,19 @@ public class SignupScreen extends AppCompatActivity {
             {   //toast is a short popup message that pops up near the bottom of the screen
                 Toast.makeText(SignupScreen.this, "These are the Terms & Conditions", Toast.LENGTH_SHORT).show();
 
-                //this cancels the checkbox being checked if clicking on T&C's
+                //this stops the checkbox being checked if clicking on T&C's
                 widget.cancelPendingInputEvents();
             }
         };
         ssClickable.setSpan(clickableSpan, 17, 35, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        checkBox.setText(ssClickable);
-        checkBox.setMovementMethod(LinkMovementMethod.getInstance());
+        termsConditions.setText(ssClickable);
+        termsConditions.setMovementMethod(LinkMovementMethod.getInstance());
 
+    }
 
+    @Override
+    public void onClick(View view) {
+
+        Toast.makeText(SignupScreen.this, "Go Back to Login/SignUp Screen", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/test/java/com/aut/parkit/SignUpScreenTests/TermsConditionsTest.java
@@ -0,0 +1,30 @@
+package com.aut.parkit.SignUpScreenTests;
+
+import com.aut.parkit.View.SignupScreen;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TermsConditionsTest extends TestCase
+{
+    //work out exactly what you want to test first.  the exact logic you are looking to validate
+
+    //app should display the full terms and conditions when clicking on the link
+    //sign up button should be enabled when agreeing to T&C's
+    //sign up button should not be enabled when T&C's checkbox not ticked
+
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Test
+    public void checker()
+    {
+
+    }
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
