f1f9e0c6b8888c840cff6b487e60eb4989269f27
BradRautenbach <54045202+BradRautenbach@users.noreply.github.com>
2019-09-06 11:57:48 +1200

    Created a loggin class to test the login function

---
+++ app/src/main/java/com/aut/parkit/View/LoginTestActivity.java
@@ -0,0 +1,14 @@
+package com.aut.parkit.View;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.aut.parkit.R;
+
+public class LoginTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_test);
+    }
+}
