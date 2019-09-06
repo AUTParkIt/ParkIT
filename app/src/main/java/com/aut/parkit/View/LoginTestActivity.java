9f6eff159a2df2da709005dcbbb5608b8fd8d876
BradRautenbach <54045202+BradRautenbach@users.noreply.github.com>
2019-09-06 12:15:11 +1200

    Merging Rename branch

--- app/src/main/java/com/aut/parkit/View/LoginTestActivity.java
+++ app/src/main/java/com/aut/parkit/View/LoginTestActivity.java
@@ -1,10 +1,15 @@
 package com.aut.parkit.View;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import com.aut.parkit.R;
 
 public class LoginTestActivity extends AppCompatActivity {
+
+    private Button loginButton, signUpButton;
+    private EditText emailText, passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
