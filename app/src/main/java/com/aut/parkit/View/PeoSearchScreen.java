1a67c007f0d5832c3adb755cc720c2151f09552b
nlquill <52065976+nlquill@users.noreply.github.com>
2019-09-07 22:57:09 +1200

    PEO search screen added

---
+++ app/src/main/java/com/aut/parkit/View/PeoSearchScreen.java
@@ -0,0 +1,28 @@
+package com.aut.parkit.View;
+
+import android.os.Bundle;
+import androidx.appcompat.app.ActionBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import com.aut.parkit.R;
+
+public class PeoSearchScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peo_search_screen);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME);
+        getSupportActionBar().setCustomView(R.layout.actionbar_title);
+
+
+    }
+
+    public void onSearchSubmit(){
+
+    }
+
+}
