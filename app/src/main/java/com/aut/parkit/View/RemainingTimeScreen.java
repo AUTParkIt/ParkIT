d6b9f6bbf0540ce6ca61bb5505fbb8594e4d74dc
nlquill <natrat13@autuni.ac.nz>
2019-09-13 11:01:04 +1200

    Remaining time screen added

--- app/src/main/java/com/aut/parkit/View/RemainingTimeScreen.java
+++ app/src/main/java/com/aut/parkit/View/RemainingTimeScreen.java
@@ -1,8 +1,11 @@
 package com.aut.parkit.View;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.aut.parkit.R;
 
@@ -11,6 +14,31 @@ public class RemainingTimeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME);
+        getSupportActionBar().setCustomView(R.layout.actionbar_title);
         setContentView(R.layout.activity_remaining_time_screen);
+    }
+
+    //SETTINGS MENU METHODS:
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_myaccount) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
