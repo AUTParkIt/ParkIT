1c3fbdbc371336381f53a6bcc016a05689179a19
zureen <zureenhanif07@gmail.com>
2019-09-13 10:54:32 +1200

    Merging11

--- app/src/main/java/com/aut/parkit/View/LoginScreen.java
+++ app/src/main/java/com/aut/parkit/View/LoginScreen.java
@@ -1,5 +1,6 @@
 package com.aut.parkit.View;
 
+import android.content.Intent;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
@@ -9,20 +10,37 @@ import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 import com.aut.parkit.R;
 
 public class LoginScreen extends AppCompatActivity
 {
+    TextView forgotpw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-        TextView textView2 = findViewById(R.id.textView2);
-        textView2.setOnClickListener(new View.OnClickListener() {
+        forgotpw = findViewById(R.id.textView);
+        forgotpw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                open();
+                Toast toast =
+                        Toast.makeText(getApplicationContext(),
+                        "Password Restoration Link \nhas been sent to your email!",
+                        Toast.LENGTH_LONG);
+                toast.show();
+                //opens Forgot Password PopUp;
+            }
+        });
+
+        TextView signup = findViewById(R.id.textView2);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //openSignUp();
+                //startActivity(new Intent(LoginScreen.this, SignIn.class));
             }
         });
 
@@ -30,14 +48,18 @@ public class LoginScreen extends AppCompatActivity
         SpannableString ss = new SpannableString(text);
         ForegroundColorSpan fcsBlack = new ForegroundColorSpan(Color.BLACK);
         ss.setSpan(fcsBlack, 8,10, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        textView2.setText(ss);
+        signup.setText(ss);
 
     }
+
+  /*  public void openSignUp() {
+        Intent intent = new Intent(this, SignUp.class);
+        startSignUp(intent);
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mailbox, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
-
 }
