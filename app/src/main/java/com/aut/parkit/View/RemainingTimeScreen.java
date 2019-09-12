8f9f611f68c17fbf25fbd5c6343752ab1fc5ff4a
nlquill <natrat13@autuni.ac.nz>
2019-09-13 10:04:43 +1200

    Remaining time screen added

---
+++ app/src/main/java/com/aut/parkit/View/RemainingTimeScreen.java
@@ -0,0 +1,16 @@
+package com.aut.parkit.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aut.parkit.R;
+
+public class RemainingTimeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remaining_time_screen);
+    }
+}
