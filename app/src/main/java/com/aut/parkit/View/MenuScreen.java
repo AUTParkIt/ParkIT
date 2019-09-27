d3a8f167ddfd95e7970ee7e14dd594c4a7aea7fe
ACRSlaven <54045183+ACRSlaven@users.noreply.github.com>
2019-09-27 19:21:14 +1200

    Menu Screen

--- app/src/main/java/com/aut/parkit/View/MenuScreen.java
+++ app/src/main/java/com/aut/parkit/View/MenuScreen.java
@@ -1,6 +1,8 @@
 package com.aut.parkit.View;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +11,50 @@ import com.aut.parkit.R;
 
 public class MenuScreen extends AppCompatActivity {
 
+    private Button myAcc, myPay, myCars, tAndCs, logOut;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME);
         getSupportActionBar().setCustomView(R.layout.actionbar_title);
         setContentView(R.layout.activity_menu_screen);
+
+        myAcc = findViewById(R.id.myAccountBtn);
+        myPay = findViewById(R.id.payMethodBtn);
+        myCars = findViewById(R.id.garageBtn);
+        tAndCs = findViewById(R.id.termsBtn);
+        logOut = findViewById(R.id.logOutBtn);
+
+        myAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        myPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        myCars.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        tAndCs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
